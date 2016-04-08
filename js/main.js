d2c5f49a8533f02c318f122aea115ff39ecff8af
SarahDowling <SarahandMac2@Sarahs-MacBook-Pro.local>
2016-04-07 22:50:09 -0400

    added div for star, and made star appear

--- js/main.js
+++ js/main.js
@@ -2,3 +2,11 @@ var $body = $('.banner');
 var $star;
 var width = $body.outerWidth();
 var height = $body.outerHeight();
+
+for (i = 0; i < 75; i++) {
+  $star = $('<div>');
+  $star.addClass('star');
+
+   });
+  $body.append($star);
+}
